50e37fb71e4cab2af878d6f520f17c0d21a55342
snowcream <npq1@hi.is>
2017-09-06 20:04:57 +0000

    Adding documentation for the functions

--- src/main/java/teymi15/assignment1/control/mainpageController.java
+++ src/main/java/teymi15/assignment1/control/mainpageController.java
@@ -1,6 +1,11 @@
 package teymi15.assignment1.control;
-
-
+/**
+* The program allows user the to write in their name in the input 
+* and it will displayed their name with the greeting "Good Morning" + their name
+* @author  Nu Phan Quynh Do
+* @version 1.0
+* @since   2017-09-5 
+*/
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -8,14 +13,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+* The class controls the main page to tells which route it should be rending
+* and what information it should be displayed
+*/
 @Controller
 public class mainpageController {
-
-    @RequestMapping
+    
+    /**
+    * The function tells the initial page to be displayed at route predefined
+    * in the configuration application.properties
+    * @param Model - model
+    * @return String
+    */  
+    @RequestMapping("/")
     public String initialPage(Model model) {
         model.addAttribute("name", " ");
         return "mainpage";
     }
+    
+    /**
+    * The function tells the same route should be rendered when the user confirm 
+    * the input. Furthermore, when getting the input from the user the page should
+    * be displaying the information back to the page.
+    * @param String - name 
+    * @param Modelmap - modelMapel
+    * @return String
+    */
     @RequestMapping(value="/", method = RequestMethod.POST)
     public String changeDisplayedName(@RequestParam(value = "name", required = false) String name, ModelMap modelMapel) {
         modelMapel.addAttribute("name", name);
